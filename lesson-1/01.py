eee827e3e1e52357ecb5db96fb553d6f2b532397
Roman Balandin <tinkerscript@yandex.ru>
2022-11-22 02:44:46 +0500

    lesson-1

---
+++ lesson-1/01.py
@@ -0,0 +1,14 @@
+count = 42
+print(count)
+
+password = 'qwerty'
+print(password)
+
+isSecuredPassword = False
+print(isSecuredPassword)
+
+number = int(input('Введите число: '))
+print(number)
+
+string = input('Введите строку: ')
+print(string)
---
+++ lesson-1/03.py
@@ -0,0 +1,3 @@
+number = int(input('Введите число n: '))
+string = str(number)
+print(number + int(string + string) + int(string + string + string))
